a2f525a0fbe6dc64fe0b76015bb3112f05e51483
Richard Harrington <harrington.richard@gmail.com>
2015-06-05 02:02:12 -0400

    abstracted out boundedWord regexp function

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,9 +4,13 @@ window.Dom = (function() {
     return Object.prototype.toString.call(obj) == '[object String]';
   }
 
+  function boundedWordRegExp(word) {
+    return new RegExp('\\b' + word + '\\b');
+  }
+
   function toggleClass(el, classToToggle) {
     var className = el.className;
-    var classToToggleRegExp = new RegExp('\\b' + classToToggle + '\\b');
+    var classToToggleRegExp = boundedWordRegExp(classToToggle);
     if (className.match(classToToggleRegExp)) {
       el.className = className.replace(classToToggleRegExp, '').trim();
     }
@@ -17,7 +21,7 @@ window.Dom = (function() {
 
   function addClass(el, classToAdd) {
     var className = el.className;
-    var classToAddRegExp = new RegExp('\\b' + classToAdd + '\\b');
+    var classToAddRegExp = boundedWordRegExp(classToToggle);
     if (!className.match(classToAddRegExp)) {
       el.className = (className + ' ' + classToAdd).trim();
     }
