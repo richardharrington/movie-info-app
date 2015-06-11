de559469f762c9ce6cf18021386c10837a086911
Richard Harrington <harrington.richard@gmail.com>
2015-06-11 15:16:43 -0400

    separated main into module to avoid IIFE

--- with_libraries/build.sh
+++ with_libraries/build.sh
@@ -2,7 +2,7 @@
 rm -rf dist
 mkdir -p dist/public/js
 mkdir -p dist/public/css
-browserify public/js/main.js > dist/public/js/main.js
+browserify public/js/index.js > dist/public/js/index.js
 cp -R public/css/* dist/public/css/.
 cp server.js dist/server.js
 echo "[]" > dist/data.json
