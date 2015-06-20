77d3b6b79f474dd5a8802dabfa904398b5ada7e0
Richard Harrington <harrington.richard@gmail.com>
2015-06-20 06:55:40 -0400

    abstracted out server 'database' into its own object

--- react/server.js
+++ react/server.js
@@ -4,14 +4,37 @@ var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var DATA_FILE_NAME = 'data.json';
+
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/favorites', function(req, res){
-  var data = fs.readFileSync(path.join (__dirname, 'data.json'));
+var store = (function(dataFileName) {
+  var dataFilePath = path.join(__dirname, dataFileName);
+  var read = function() {
+    return JSON.parse(fs.readFileSync(dataFilePath));
+  }
+  var write = function(data) {
+    fs.writeFileSync(dataFilePath, JSON.stringify(data));
+  }
+  var get = read;
+  var add = function(newRecord) {
+    var data = read();
+    data.push(newRecord);
+    write(data);
+  }
+  return {add: add, get: get};
+})(DATA_FILE_NAME);
+
+var sendAsJson = function(res, data) {
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
+}
+
+app.get('/favorites', function(req, res){
+  var data = store.get();
+  sendAsJson(res, data);
 });
 
 app.post('/favorites', function(req, res){
@@ -19,12 +42,8 @@ app.post('/favorites', function(req, res){
     res.status(400).send({error: "JSON not formed properly"});
     return;
   }
-  var dataFilePath = path.join(__dirname, 'data.json');
-  var data = JSON.parse(fs.readFileSync(dataFilePath));
-  data.push(req.body);
-  fs.writeFileSync(dataFilePath, JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  store.add(req.body);
+  sendAsJson(res, data);
 });
 
 app.listen(3000, function(){
