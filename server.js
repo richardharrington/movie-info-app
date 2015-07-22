fe1294c1612c225183d061ee4c8c36fd3d25176a
Richard Harrington <harrington.richard@gmail.com>
2015-07-22 00:26:54 -0400

    moved port code around, standardized default to 3000

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+app.set('port', (process.env.PORT || 3000));
+
 var DATA_FILE_NAME = 'data.json';
 
 app.use(express.static(path.join(__dirname, 'src')));
@@ -80,9 +82,6 @@ app.get('/env', function(req, res) {
 })
 
 store.init();
-
-
-app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function(){
   console.log("Listening on port", app.get('port'));
